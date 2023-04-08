f4bb3fa319aa0e02e6ce0ee7fdfb234d1f2bf13e
Nick Iliopoulos <nick.iliopoulos@gmail.com>
2023-04-08 19:13:33 -0400

    v0.04: added comments

--- src/Components/KeyboardInput2.js
+++ src/Components/KeyboardInput2.js
@@ -1,15 +1,26 @@
+// import React component, and the useEffect and useState hooks
 import React, { useEffect, useState } from 'react';
 
+/*  this function component places an event listener on the entire web
+    document that listens for keydown events, and handles these events
+**/ 
 function KeyboardInput2 () {
+
+    // set a state variable and it's value setter function with the useState hook
+    // set the initial value of the state variable to ""
     const[inputValue, setInputValue] = useState("");
 
+    // the useEffect hook is a lifecycle hook that handles actions when the
+    // component is mounted - (is rendered)
+    // this useEffect hook is telling this component to handle a keydown event
     useEffect ( () => {
+        // how to handle the keydown event
         function handleKeyDown(event) {
             console.log(event.key);
             const {key} = event;
             setInputValue(prevValue=>prevValue + key);
         }
-        
+        // listen for keydown events on the entire document
         document.addEventListener('keydown',handleKeyDown);
 
         // clean up
