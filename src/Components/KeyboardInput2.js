6ba0c7da77c217f9292f7461e319ebed59b2adc0
Nick Iliopoulos <nick.iliopoulos@gmail.com>
2023-04-11 16:45:29 -0400

    v0.09: allow backspace when max length is reached

--- src/Components/KeyboardInput2.js
+++ src/Components/KeyboardInput2.js
@@ -9,6 +9,7 @@ function KeyboardInput2 () {
     // set a state variable and it's value setter function with the useState hook
     // set the initial value of the state variable to ""
     const[inputValue, setInputValue] = useState("");
+    const[wordArray, setWordArray] = useState([]);
 
     // the useEffect hook is a lifecycle hook that handles actions when the
     // component is mounted - (is rendered)
@@ -16,46 +17,60 @@ function KeyboardInput2 () {
     useEffect ( () => {
         // how to handle the keydown event
         function handleKeyDown(event) {
-            if (inputValue.length >= 6){
+            // get current inputValue
+            const {key} = event;
+            const k = event.which;
+            /*
+            console.log(event.key);
+            console.log("k: "+k);
+            */
+            var pattern = /[A-Za-z]/;
+            // handle backspace key
+            if (k === 8){
+                setInputValue(prevValue => prevValue.slice(0, -1));
+                setWordArray(wArray => wArray.slice(0, -1));
+            }
+            else if (inputValue.length >= 6){
                 console.log("Max length reached: "+inputValue.length);
+                console.log("inputValue: " + inputValue);
+                console.log("inputValue.length: " + inputValue.length);
+                
+                console.log("wordArray: " + wordArray);
+                console.log("wordArray.length: " + wordArray.length);
                 // exit
                 return true;
             }
-            // get current inputValue
-            console.log(event.key);
-            const {key} = event;
-            const k = event.which;
-            console.log("k: "+k);
-            var pattern = /[A-Za-z]/;
             // Verify that the key entered is not a special key
-            if (k == 20 /* Caps lock */
-                || k == 16 /* Shift */
-                || k == 46 /* Delete */
-                || k == 9 /* Tab */
-                || k == 27 /* Escape Key */
-                || k == 17 /* Control Key */
-                || k == 91 /* Windows Command Key */
-                || k == 19 /* Pause Break */
-                || k == 18 /* Alt Key */
-                || k == 93 /* Right Click Point Key */
+            else if (k === 20 /* Caps lock */
+                || k === 16 /* Shift */
+                || k === 46 /* Delete */
+                || k === 9 /* Tab */
+                || k === 27 /* Escape Key */
+                || k === 17 /* Control Key */
+                || k === 91 /* Windows Command Key */
+                || k === 19 /* Pause Break */
+                || k === 18 /* Alt Key */
+                || k === 93 /* Right Click Point Key */
                 || ( k >= 35 && k <= 40 ) /* Home, End, Arrow Keys */
-                || k == 45 /* Insert Key */
+                || k === 45 /* Insert Key */
                 || ( k >= 33 && k <= 34 ) /*Page Down, Page Up */
                 || (k >= 112 && k <= 123) /* F1 - F12 */
                 || (k >= 144 && k <= 145 )) { /* Num Lock, Scroll Lock */
             }
-            // handle backspace key
-            else if (k == 8){
-                setInputValue(prevValue => prevValue.slice(0, -1))
-            }
             // handle enter key
-            else if (k == 13) {
+            else if (k === 13) {
                 setInputValue(prevValue => prevValue + '\r\n');
             }
             else if (pattern.test(key)){
-                setInputValue(prevValue=>prevValue + key);
+                setInputValue(prevValue => prevValue + key);
+                setWordArray(wArray => wArray.concat(key));
             }
             console.log("inputValue: " + inputValue);
+            console.log("inputValue length: " + inputValue.length);
+            
+            console.log("wordArray:" + wordArray);
+            console.log("wordArray.length:" + wordArray.length);
+
         }
         
         // listen for keydown events on the entire document
@@ -65,7 +80,7 @@ function KeyboardInput2 () {
         return function cleanup () {
             document.removeEventListener('keydown', handleKeyDown);
         }
-    }, [inputValue]);
+    }, [inputValue, wordArray]);
 
     return (
         <div>
